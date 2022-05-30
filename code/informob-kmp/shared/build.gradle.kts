f88ebb59f335089ea163fb41c979d2b58496371a
Christopher Augustus <repos@c4augustus.com>
2022-05-30 11:33:22 +0100

    remove all src/.../kotlin subdirectories, moving their files up to the parent directory; NOTE that this will produce an Android Studio sync warning "Duplicate content roots detected", but this is the only way to execute unit tests in shared/src/androidTest

--- code/informob-kmp/shared/build.gradle.kts
+++ code/informob-kmp/shared/build.gradle.kts
@@ -17,18 +17,18 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
+        val commonMain by getting { kotlin.srcDir("src/commonMain") }
+        val commonTest by getting { kotlin.srcDir("src/commonTest")
             dependencies {
                 implementation(kotlin("test"))
             }
         }
-        val androidMain by getting
-        val androidTest by getting
+        val androidMain by getting { kotlin.srcDir("src/androidMain") }
+        val androidTest by getting { kotlin.srcDir("src/androidTest") }
         val iosX64Main by getting
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
-        val iosMain by creating {
+        val iosMain by creating { kotlin.srcDir("src/iosMain")
             dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
@@ -37,7 +37,7 @@ kotlin {
         val iosX64Test by getting
         val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
-        val iosTest by creating {
+        val iosTest by creating { kotlin.srcDir("src/iosTest")
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
