d2669fd1a94c5fffc9d972050c8dea2a914f4626
Christopher Augustus <repos@c4augustus.com>
2022-06-29 16:35:36 +0100

    move the bulk of MainActivity code (generated from the Jetpack Compose template) into AppFlowAnd.kt actual function viewApp(AppMutant) so that MainActivity::onCreate(...) delegates through common function enterApp(AppMutant) passing the activity itself as the platform blob

--- code/informob-kmp/shared/src/androidMain/side/AppFlowAnd.kt
+++ code/informob-kmp/shared/src/androidMain/side/AppFlowAnd.kt
@@ -1,4 +1,43 @@
 package org.informob.side
 
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+import org.informob.ui.theme.InformobTheme
+
 actual fun viewApp(appMutant: AppMutant): Boolean =
-   false
+    (appMutant.platformBlobRef as? ComponentActivity)?.run {
+        setContent { // !!! will not work outside of Activity class
+            InformobTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+        true
+    } ?: false
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    InformobTheme {
+        Greeting("Android")
+    }
+}
--- code/informob-kmp/shared/src/androidMain/side/MainActivity.kt
+++ code/informob-kmp/shared/src/androidMain/side/MainActivity.kt
@@ -2,47 +2,11 @@ package org.informob.side
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import org.informob.Greeting
-import org.informob.ui.theme.InformobTheme
-
-fun greet(): String {
-    return Greeting().greeting()
-}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            InformobTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        enterApp(AppMutant(platformBlobRef = this))
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    InformobTheme {
-        Greeting("Android")
-    }
-}
