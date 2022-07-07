5f8ef57a8874142a16acb90d5b3846bd60c96619
Christopher Augustus <repos@c4augustus.com>
2022-07-07 19:07:37 +0100

    enhance iosApp with a new SwiftUI that shows the platform OS stats

--- code/informob-kmp/iosApp/src/main/side/ContentView.swift
+++ code/informob-kmp/iosApp/src/main/side/ContentView.swift
@@ -1,12 +1,29 @@
+// Copyright 2022 Christopher Augustus
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
 import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greeting()
+    let osStats = OsStatsPresentKt.osStatsPresent(
+        model: PlatformIosKt.platformOsStats())
 
-	var body: some View {
-		Text(greet)
-	}
+    var body: some View {
+        VStack {
+            VStack {
+                Text(osStats.name)
+                    .font(.largeTitle)
+                Text(osStats.version)
+                    .font(.title)
+            }
+            .padding()
+        }
+        .background(Color.gray)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
