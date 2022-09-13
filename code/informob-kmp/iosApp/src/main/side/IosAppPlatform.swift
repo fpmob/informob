c7c55b21ef486428772469873f3b53f4762a5e12
Christopher Augustus <repos@c4augustus.com>
2022-09-13 16:33:16 +0100

    create iosAppPlatformFunctions() with placeholder TODO for presentMainScreen(...)

---
+++ code/informob-kmp/iosApp/src/main/side/IosAppPlatform.swift
@@ -0,0 +1,17 @@
+// Copyright © 2022 Christopher Augustus
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import shared
+
+public func iosAppPlatformFunctions() -> AppPlatformFunctions {
+    AppPlatformFunctions(
+        presentMainScreen: { (appMutant: AppMutant) -> AppMutant in
+            // TODO: @@@ REFACTOR MainApp.body COMPOSE UI CODE TO HERE
+            return appMutant
+        }
+    )
+}
