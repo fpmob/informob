e40e2b8140bd67a5bfacccc18b73f1170c4a3a64
Christopher Augustus <repos@c4augustus.com>
2024-09-05 23:19:45 +0000

    change kmp shared Android color theme secondary from Teal200 to Gray200

--- code/informob-kmp/shared/src/androidMain/ui/theme/Color.kt
+++ code/informob-kmp/shared/src/androidMain/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package org.informob.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Gray200 = Color(0xFFDADADA)
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
