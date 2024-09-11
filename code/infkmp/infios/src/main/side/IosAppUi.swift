6f2da81957143c8fdc7d12cbebbeb8f0e9b78287
Christopher Augustus <repos@c4augustus.com>
2024-09-11 20:34:27 +0000

    evolve IosAppUi: buttons for "Swift UI" and "Uinota" that toggle vertical panels below with placeholder colors and a thick border

--- code/infkmp/infios/src/main/side/IosAppUi.swift
+++ code/infkmp/infios/src/main/side/IosAppUi.swift
@@ -9,23 +9,69 @@ import SwiftUI
 import infsha
 
 struct AppView: View {
-    let osStats = OsStatsPresentKt.osStatsPresent(
-        model: PlatformIosKt.platformOsStats())
-
+    @State private var hasSwiftUi = false
+    @State private var hasUinota  = false
+    let spacing = 16.0
+    let buttonx = 80.0
     var body: some View {
         VStack {
-            VStack {
-                Text(osStats.name)
-                    .font(.largeTitle)
-                Text(osStats.version)
-                    .font(.title)
-            }
-            .padding()
+            HStack {
+                OsStatsView()
+                Spacer()
+                VStack(spacing: spacing) {
+                    Button {
+                        hasSwiftUi = !hasSwiftUi
+                    } label: { ButtonViewText(
+                        text: "Swift UI", x:buttonx, y:spacing) }
+                    Button {
+                        hasUinota = !hasUinota
+                    } label: { ButtonViewText(
+                        text: "Uinota"  , x:buttonx, y:spacing) }
+                }.padding()
+                //.border(Color.yellow, width: 1)
+                Spacer()
+            }//.border(Color.yellow, width: 1)
+            if hasSwiftUi {
+                ZStack { Color.blue  }.border(Color.orange, width: 4)
+            } else { Spacer() }
+            if hasUinota {
+                ZStack { Color.green }.border(Color.orange, width: 4)
+            } else { Spacer() }
         }
-        .background(Color.gray)
+        .background(Color.black)
+        //.border(Color.yellow, width: 1)
     }
 }
 
 struct AppView_Previews: PreviewProvider {
 	static var previews: some View { AppView() }
+}
+
+struct ButtonViewText: View {
+    var text: String
+    var x, y: CGFloat
+    var body: some View { Text(text)
+        .bold()
+        .frame(width: x, height: y)
+        .padding()
+        .background(     Color.orange)
+        .foregroundColor(Color.black)
+        .cornerRadius(y)
+        //.border(Color.yellow, width: 1)
+    }
+}
+
+struct OsStatsView: View {
+    let osStats = OsStatsPresentKt.osStatsPresent(
+        model: PlatformIosKt.platformOsStats())
+    var body: some View {
+        VStack {
+            Text(osStats.name   ).font(.largeTitle)
+            Text(osStats.version).font(.title)
+        }
+        .padding()
+        .background(Color.purple)
+        .foregroundColor(Color.white)
+        //.border(Color.yellow, width: 1)
+    }
 }
