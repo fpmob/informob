e8875b82bd3b2fe2e03b1037e389b1f1163b82d6
Christopher Augustus <repos@c4augustus.com>
2025-01-19 22:48:42 +0000

    complete Android jpc to match iOS sui, the top OS stats and panel buttons

--- code/infkmp/infsha/src/androidMain/side/JpcViewMain.kt
+++ code/infkmp/infsha/src/androidMain/side/JpcViewMain.kt
@@ -7,16 +7,17 @@
 
 package org.informob.side
 
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.*
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 
 import org.informob.pure.present.*
 import org.informob.ui.theme.jpcThemeInf
@@ -29,39 +30,93 @@ fun andPresentMainScreen(appMutant: AppMutant): AppMutant = appMutant.apply {
     }
 }
 
+val colorBackDraw   = Color(red=0.7f,green=0.6f,blue=0.6f)
+val colorBackOs     = Color(red=0.2f,green=0.2f,blue=0.2f)
+val colorBackPerf   = Color(red=0.7f,green=0.7f,blue=0.6f)
+val colorBackRandom = Color(red=0.6f,green=0.7f,blue=0.6f)
+val colorBackStress = Color(red=0.6f,green=0.7f,blue=0.7f)
+val colorBordDebug  = Color(red=1.0f,green=1.0f,blue=0.0f) // .yellow
+val colorBordDraw   = Color(red=0.6f,green=0.0f,blue=0.0f)
+val colorBordPerf   = Color(red=0.6f,green=0.4f,blue=0.0f)
+val colorBordRandom = Color(red=0.0f,green=0.5f,blue=0.0f)
+val colorBordStress = Color(red=0.0f,green=0.2f,blue=0.7f)
+val colorForeDraw   = Color(red=0.6f,green=0.3f,blue=0.3f)
+val colorForeOs     = Color(red=0.7f,green=0.7f,blue=0.7f)
+val colorForePerf   = Color(red=0.6f,green=0.4f,blue=0.2f)
+val colorForeRandom = Color(red=0.6f,green=0.7f,blue=0.6f)
+val colorForeStress = Color(red=0.4f,green=0.4f,blue=0.8f)
+
+val paddingCommon   = 16
+val paddingOsStats  = 8
+val widthBordPanel  = 4.0
+
 @Composable
-fun jpcViewMain() {
-    Column(
+fun jpcViewMain()
+  = Column(
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Top,
         modifier = Modifier.fillMaxSize().background(Color.Black)
     ) {
-        jpcOsViewStats(osStatsPresent(platformOsStats()))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Column(modifier = Modifier.weight(0.36f)
+                   .padding(all = paddingCommon.dp)) {
+                jpcViewOsStats(osStatsPresent(platformOsStats()))
+            }
+            Column(modifier = Modifier.weight(0.32f)
+                    .padding(top = paddingCommon.dp,
+                             end = paddingCommon.dp,
+                          bottom = paddingCommon.dp)) {
+                jpcButtonPanel("Draws",  colorBackDraw    ) { }
+                jpcButtonPanel("Random", colorBackRandom) { }
+            }
+            Column(modifier = Modifier.weight(0.32f)
+                    .padding(top = paddingCommon.dp,
+                             end = paddingCommon.dp,
+                          bottom = paddingCommon.dp)) {
+                jpcButtonPanel("Perfs",  colorBackPerf  ) { }
+                jpcButtonPanel("Stress", colorBackStress) { }
+            }
+        }
     }
-}
-
-@Composable
-fun jpcOsViewStats(model: OsStatsPresent) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .background(Color(160, 80, 160)) // purple
-            .padding(all = 12.dp)
-    ) {
-        Text(
-            text = model.name,
-            color = Color.White,
-            style = MaterialTheme.typography.subtitle2)
-        Text(
-            text = model.version,
-            color = Color.White,
-            style = MaterialTheme.typography.subtitle1)
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
-fun jpcViewMainPreview() {
-    jpcThemeInf {
+fun jpcViewMainPreview()
+  = jpcThemeInf {}
+
+@Composable
+fun jpcButtonPanel(
+    label: String,
+    color: Color,
+    action: () -> Unit
+) =
+    Button({ action },
+        shape = (2f * paddingCommon).let { RoundedCornerShape(it, it, it, it) },
+        colors = ButtonDefaults.buttonColors(backgroundColor = color),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(text = label,
+            letterSpacing = 0.sp,
+            overflow = TextOverflow.Clip,
+            style = MaterialTheme.typography.button)
     }
-}
+
+@Composable
+fun jpcViewOsStats(model: OsStatsPresent)
+  = Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(colorBackOs)
+            .padding(all = paddingOsStats.dp)
+    ) {
+        Text(
+            text      = model.name,
+            color     = colorForeOs,
+            textAlign = TextAlign.Center,
+            style     = MaterialTheme.typography.subtitle2)
+        Text(
+            text      = model.version,
+            color     = colorForeOs,
+            textAlign = TextAlign.Center,
+            style     = MaterialTheme.typography.subtitle1)
+    }
